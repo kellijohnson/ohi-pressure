0ea6d42d77051f3c7b9eaeae4a0daaf5bbc924c2
Kelli Johnson <kellifayejohnson@gmail.com>
2017-10-02 15:05:42 -0700

    change loops to parallel and fix error, move documentation

--- ohi-pressure_sst.R
+++ ohi-pressure_sst.R
@@ -1,14 +1,6 @@
 # Selig, E.R., K.S. Casey, and J.F. Bruno. 2010. New insights into global patterns of ocean temperature anomalies: implications for coral reef health and management. Global Ecology and Biogeography, DOI: 10.1111/j.1466-8238.2009.00522.x.
 # https://cdn.rawgit.com/OHI-Science/ohiprep/master/globalprep/prs_sst/v2016/sst_layer_prep.html
 
-# **Native Data Resolution**: 4km
-# **Description**:
-# Cortadv5_SSTA.nc = SST anomalies
-# (weekly SST minus weekly climatological SST),
-# weekly data for all years, degrees Kelvin
-# Cortadv5_weeklySST.nc =  SST, weekly data for all years, degrees Kelvin
-# **Time Range**: 1982 - 2012 (weekly averages across all years)
-# **Format**: NetCDF
 # #Methods
 # 1. Extreme events per year based calculated as number of times SST anomaly
 # exceeds SST Standard Deviation based on weekly values
@@ -19,14 +11,22 @@
 # 4. Rescale "Change in extreme events" data to values between 0 and 1 by
 # dividing by the 99.99th quantile among all years of data.
 
+# **Format**: NetCDF
+# **Native Data Resolution**: 4km
+# Data was obtained from nodc.noaa.gov/sog/cortad V5
+# doi:10.7289/V5CZ3545
+# Three dimensions (time, latitude, and longitude), where the
+# dimensions are c(1617, 4320, 8640) and time is the middle of the week.
+# One entry for every week since 1982-01-02
+# Cortadv5_SSTA.nc = SST anomalies
+# (weekly SST minus weekly climatological SST),
+# weekly data for all years, degrees Kelvin
+# Cortadv5_weeklySST.nc =  SST, weekly data for all years, degrees Kelvin
+# **Time Range**: 1982 - 2012 (weekly averages across all years)
+
 ###############################################################################
 #### Prep raw sea surface temperature (CoRTAD version 5) for OHI 2015
 ###############################################################################
-# Data was obtained from nodc.noaa.gov/sog/cortad V5, which is a NetCDF file
-# doi:10.7289/V5CZ3545
-# The nc file has three dimensions (time, latitude, and longitude), where the
-# dimensions are c(1617, 4320, 8640) and time is the middle of the week.
-# One entry for every week since 1982-01-02
 file_sst <- file.path(dir.data, "cortadv5_WeeklySST.nc")
 file_ssta <- file.path(dir.data, "cortadv5_SSTA.nc")
 
@@ -36,7 +36,7 @@ setwd(file.path(dir_M,"git-annex/globalprep/prs_sst"))
 
 dir.temp <- file.path(dir.data, "temp")
 dir.create(dir.temp, showWarnings = FALSE)
-rasterOptions(dir.temp = dir.temp)
+raster::rasterOptions(tmpdir = dir.temp)
 
 ssta         <- stack(file_ssta, varname = "SSTA")
 weekly_sst   <- stack(file_sst, varname = "WeeklySST")
@@ -44,7 +44,7 @@ names_ssta   <- names(ssta)
 names_weekly <- names(weekly_sst)
 
 #Create weekly standard deviations across all years
-for(i in 1:53) {
+foreach(i = 1:53) %dopar% {
   s <- stack()
   for (j in 1982:2012) {
     w <- which(substr(names_weekly, 2, 5) == j)[i]
@@ -53,14 +53,15 @@ for(i in 1:53) {
     s <- stack(s, w_week)
   }
   sd <- calc(s, fun = function(x){sd(x, na.rm = TRUE)},
-    progress = "text", filename = paste("sd_sst_week_", i,".tif"))
+    progress = "text", 
+    filename = file.path(dir.data, paste("sd_sst_week_", i,".tif")))
 }
 
 # Calculate annual positive anomalies
-for (i in 1982:2012) {
+foreach(i = 1982:2012) %dopar% {
   s <- stack()
   for (j in 1:53) {
-    sd <- raster(paste0("tmp/sd_sst_week_",j,".tif")) #sd for week
+    sd <- raster(file.path(dir.data, paste0("sd_sst_week_",j,".tif"))) #sd for week
     w <- which(substr(names_ssta, 2, 5) == i)[j]
     if(is.na(w)) next()
     # subset the week/year anomaly
@@ -79,23 +80,22 @@ for (i in 1982:2012) {
 ###############################################################################
 #### 5-year averages and differences from the mean
 ###############################################################################
-l   <- list.files(dir.temp,
+l <- list.files(dir.temp,
   pattern = "annual_pos_anomalies", full.names = TRUE)
 
 # Get 5 year aggregates
   # 5-year historical comparison
   ref <- stack(l[4:8]) %>% sum(.)
-  registerDoParallel(10)
 
 foreach(i = 1986:2008) %dopar% {
   yrs <- c(i:(i+4))
-  s   <- stack(l[substr(l,81,84)%in%yrs])%>%sum(.)
+  s   <- stack(l[substr(l,81,84)%in%yrs]) %>% sum(.)
+  #calc diff between recent 5 year mean and ref
   diff <- overlay(s, ref,
-    fun = function(x,y){x-y}) %>% #calc diff between recent 5 year mean and ref
-    mask(land, inverse = TRUE)
+    fun = function(x,y){x-y}) %>% mask(land, inverse = TRUE)
   writeRaster(diff,
     filename = file.path(dir.data,
-      paste0("sst_diff_ocean_", yrs[1], "-", yrs[5],".tif"),
+      paste0("sst_diff_ocean_", yrs[1], "-", yrs[5],".tif")),
       overwrite = TRUE)
 }
 
